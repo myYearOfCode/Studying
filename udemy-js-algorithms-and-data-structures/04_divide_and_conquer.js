9a634c7276018c0ea2da4cc8e2ff465b2e876c88
Ross Daly <ross@myYearOfCode.com>
2018-10-07 22:36:57 -0400

    divide and conquer initial commit

---
+++ udemy-js-algorithms-and-data-structures/04_divide_and_conquer.js
@@ -0,0 +1,12 @@
+// divide and conquer. O( log N )
+// this pattern invoves dividing a data set into smaller chunks and then repeating 
+// a process with a subset of data.
+
+// this pattern can tremendously decrease time complexity.
+// this is used in quicksort and merge sort, also binary search trees.
+// log patterns are found when you are dividing the input array in every round.
+
+// in binary search you are looking for a number in an array of numbers. pick a point
+// (usually the middle) and see if your search number is greater or less than that number.
+// then repeat on whichever half may contain the search number.
+
