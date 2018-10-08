393058a8b1141099aba3800f5b242d34ea96bc5f
Ross <Ross@myyearofcode.com>
2018-10-08 16:26:00 -0400

    add new examples of 3 ways to check for dupes

--- udemy-js-algorithms-and-data-structures/check_for_duplicates.js
+++ udemy-js-algorithms-and-data-structures/check_for_duplicates.js
@@ -4,3 +4,33 @@ function areThereDuplicates() {
 }
 // by converting to a set you eliminate any duplicates, by checking 
 // against the original you see if there were any to start with.
+
+// these are two other ways to solve this
+
+// areThereDuplicates Solution (Frequency Counter)
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true
+  }
+  return false;
+}
+
+// areThereDuplicates Solution (Multiple Pointers)
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
