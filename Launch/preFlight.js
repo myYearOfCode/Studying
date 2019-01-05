c9ee0955d86a23e307ac2c0c6be03ec300ac6d66
Ross <Ross@myyearofcode.com>
2019-01-04 23:07:21 -0500

    add launch preflight notes

---
+++ Launch/preFlight.js
@@ -0,0 +1,11 @@
+// to convert string to int EXPLICITLY
+parseInt(stringVarName);
+// implicitly
+let newInt = (stringVarName * 1);
+// to convert an int to a string EXPLICITLY
+intVarName.toString();
+// IMPLICITLY
+let newString = intVarName + "";
+
+// popup box with text entry
+prompt("what would you like your box to say?")
