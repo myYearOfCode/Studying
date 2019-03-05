1e3911e2f89cde73c3f90d64d0b8a05ef833907a
Ross <Ross@myyearofcode.com>
2019-03-04 22:48:24 -0500

    working version

--- hackerrank/sparseArray.js
+++ hackerrank/sparseArray.js
@@ -1,23 +1,21 @@
-// I assumed it was receiving a string as input but it is receiving an array
-// that meant I didn't need to split the string into an array.
-// the site is pass/fail with no console, so I had to dig into the code to see why it was breaking. Thankfully it was an easy fix.
+//https://www.hackerrank.com/challenges/sparse-arrays/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
-function jumpingOnClouds(a) {
-    let index = 0
-    let jumps = 0
-    while (index < a.length - 1) {
-        if (a[index + 2] == "0") {
-            jumps += 1
-            index += 2
-        } else if (a[index + 1] == "0") {
-            jumps += 1
-            index += 1
-        } else {
-            console.log("no valid jumps!")
-        }
+/*
+I have no idea why this is called 'sparse array'. it doesn't seem to line up with my mental image of that. I spent a lot of time trying to do this with two nested map functions and spent so long writing an elegant solution that I overlooked a crucial detail. It isn't looking if the query string is contained in the search string, it is looking to see if they are the SAME. I spent a lot of time in regex when a simple === will do. oof.
+*/
 
-    }
-    return jumps
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+    let outArray = []
+    let results = 0
+    queries.forEach((query) => {
+        results = 0
+        strings.forEach((string) => {
+            if (query === string) {
+                results++
+            }
+        })
+        outArray.push(results)
+    })
+    return outArray
 }
-
-console.log(jumpingOnClouds("0100010".split("")))
